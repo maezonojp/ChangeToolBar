051777023c3407fb6150f0b6a1a80abdd4beeafd
maezono <maezono735@gmail.com>
2012-07-31 22:52:14 +0900

    Revert "UIBarButtonItemをタップしたらUIActivityIndicatorViewに切り替わります。"
    This reverts commit ee5222a06bb8c7363e7abc03c2025bc7ed69ae0d.

--- ChangeToolBar/AppDelegate.m
+++ ChangeToolBar/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+
 #import "ViewController.h"
 
 @implementation AppDelegate
@@ -24,19 +25,10 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
+    // Override point for customization after application launch.
     self.viewController = [[[ViewController alloc] initWithNibName:@"ViewController" bundle:nil] autorelease];
     self.window.rootViewController = self.viewController;
-    
     [self.window makeKeyAndVisible];
-    
-    //ビューコントローラー生成
-    self.viewController = [[ViewController alloc] initWithNibName:nil bundle:nil];
-    
-    //ナビゲーションコントローラー生成
-    UINavigationController *naviController = [[UINavigationController alloc] initWithRootViewController:self.viewController];
-    
-    [self.window addSubview:naviController.view];
-
     return YES;
 }
 
